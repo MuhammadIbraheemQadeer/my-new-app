c6976597c7e8b59943612d6c3caffa392aaa82aa
MuhammadIbraheemQadeer <ibrahimshaikh17124@gmail.com>
2024-11-25 22:02:40 +0500

    second commit

---
+++ my-app1/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+  <div>
+  <footer className="foot"> &copy; 2024 my-app. All Rights are Reserved. </footer>
+  </div>
+  );
+}
+
+export default Footer;
